d0818cbc87e412308e2588082487fbd1cb0174a6
Rolox <rolando.murilloaguirre@ucr.ac.cr>
2022-04-10 14:43:19 -0600

    Archivo inicial

---
+++ Repaso_General_Progra/src/main/java/Repaso_general_progra/Repaso_General_Progra.java
@@ -0,0 +1,10 @@
+package Repaso_general_progra;
+/**
+ * @author rolan
+ */
+public class Repaso_General_Progra {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
